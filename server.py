fdb3caec75d2c9298877f30edd5e367b764c9fb2
Dhairya2505 <dhairyasingla250504@gmail.com>
2024-01-11 22:49:42 +0530

    Understanding the server's Get method

--- server.py
+++ server.py
@@ -1,28 +1,18 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 
-hostName = "localhost"
-serverPort = 8080
-
-class MyServer(BaseHTTPRequestHandler):
+class echoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header('content-type','text/html')
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(self.path[1:].encode())
+
+def main():
+    PORT = 9000
+    server = HTTPServer(('',PORT),echoHandler)
+    print('Server Running on "localhost:9000"')
+    server.serve_forever()
+
 
 if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+    main()
