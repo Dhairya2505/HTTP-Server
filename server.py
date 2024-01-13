a59b7190cd67220f8091cfbdcae1e4b266b92989
Dhairya2505 <dhairyasingla250504@gmail.com>
2024-01-13 11:09:20 +0530

    add basic index.html and login.html using get method

--- server.py
+++ server.py
@@ -2,10 +2,27 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class echoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('content-type','text/html')
+        if (self.path == '/' or self.path == '/index'):
+            self.path = '/index.html'
+            try:
+                file = open(self.path[1:]).read()
+                self.send_response(200)
+            except :
+                file ='file not found'
+                self.send_response(404)
+
+        elif(self.path=='/index/login' or self.path == '/login'):
+            self.path = '/login.html'
+            try:
+                file = open(self.path[1:]).read()
+                self.send_response(200)
+            except:
+                file = 'login page not found'
+                self.send_response(404)
+
         self.end_headers()
-        self.wfile.write(self.path[1:].encode())
+        self.wfile.write(bytes(file,'utf-8'))
+            
 
 def main():
     PORT = 9000
